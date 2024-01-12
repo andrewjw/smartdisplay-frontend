51d9ce1ddcf2d0317882f263dee2d3baeef1fc4d
Andrew Wilkinson <andrewjwilkinson@gmail.com>
2024-01-12 08:45:48 +0000

    Install images.

--- install.sh
+++ install.sh
@@ -3,4 +3,7 @@
 ampy ${@:1} mkdir smartdisplay --exists-okay
 ampy ${@:1} put smartdisplay/ smartdisplay/
 
+ampy ${@:1} mkdir images --exists-okay
+ampy ${@:1} put images/ images/
+
 ampy ${@:1} put main.py main.py
