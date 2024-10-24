4b84d9172e18c5207da5180c4ddd30a251d0edf9
Andrew Wilkinson <andrewjwilkinson@gmail.com>
2024-10-24 21:50:30 +0100

    Fix style issues.

--- smartdisplay/utils.py
+++ smartdisplay/utils.py
@@ -1,9 +1,14 @@
 from i75 import I75, Image
 
-def render_image_with_fade(i75: I75, img: Image, margin: int, fade: float) -> None:
+
+def render_image_with_fade(i75: I75,
+                           img: Image,
+                           margin: int,
+                           fade: float) -> None:
     for y in range(img.height):
         for x in range(img.width):
-            if (y < margin or y >= (img.height - margin)) or (x < margin or x >= (img.width - margin)):
+            if (y < margin or y >= (img.height - margin)) \
+               or (x < margin or x >= (img.width - margin)):
                 pfade = 1.0
             else:
                 pfade = fade
