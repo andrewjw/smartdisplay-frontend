e5cb753b2dd1bf91a79ca924ad526baeb4ce4c81
Andrew Wilkinson <andrewjwilkinson@gmail.com>
2024-10-24 22:04:26 +0100

    chore: Debug github action.

--- code_style.sh
+++ code_style.sh
@@ -8,6 +8,8 @@ ln -sf `python3 -c "import importlib.util; import os.path; print(os.path.dirname
 
 ls -l
 
+ls -l `python3 -c "import importlib.util; import os.path; print(os.path.dirname(importlib.util.find_spec('i75', None).origin))"`
+
 MYPYPATH=./stubs:./emulated:$MYPYPATH mypy -m smartdisplay
 
 MYPYPATH=./stubs:./emulated:$MYPYPATH mypy main.py
