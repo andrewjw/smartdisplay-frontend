f6dc61553f548d18b37a1688247bf57ee8a0511c
Andrew Wilkinson <andrewjwilkinson@gmail.com>
2024-10-24 22:02:24 +0100

    chore: Debug github action.

--- code_style.sh
+++ code_style.sh
@@ -6,6 +6,8 @@ rm -f emulated stubs
 ln -sf `python3 -c "import importlib.util; import os.path; print(os.path.dirname(importlib.util.find_spec('i75', None).origin))"`/emulated emulated
 ln -sf `python3 -c "import importlib.util; import os.path; print(os.path.dirname(importlib.util.find_spec('i75', None).origin))"`/stubs stubs
 
+ls -l
+
 MYPYPATH=./stubs:./emulated:$MYPYPATH mypy -m smartdisplay
 
 MYPYPATH=./stubs:./emulated:$MYPYPATH mypy main.py
